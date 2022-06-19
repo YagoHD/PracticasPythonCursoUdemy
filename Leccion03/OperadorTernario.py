81525500f12002ed57927abf139be3a168369cba
Casa/Yago <yagorb@hotmail.com>
2022-06-19 09:32:06 +0200

    ADD: Leccion3

---
+++ Leccion03/OperadorTernario.py
@@ -0,0 +1,9 @@
+condicion = False
+
+# if condicion:
+#     print("Condicion verdadera")
+# else:
+#     print("Condicion falsa")
+
+#ESTA SINTAXIS SIMPLIFICADA SOLO SIRVE PARA CUANDO EL CODIGO ES PEQUEÑO Y TIENE SOLO DOS VALORES TRUE O FALSE
+print("Condicion verdadera") if condicion else print("Condicion falsa")
